df1f2cdc62679bcacbcda4a94a03d6b535af132e
Roger Braunstein <roger@partlyhuman.com>
2020-02-27 22:04:34 -0800

    Changing compression method to use imagemin with plugins instead of imagemagick. The tradeoff is no GUARANTEE of min file size, but you tend to get far superior compression. An added quality argument allows some control of this. Also, PNG support is added by giving the output file pattern a png extension.

--- batch-capture.js
+++ batch-capture.js
@@ -6,8 +6,9 @@ const program = require('commander');
 const fileUrl = require('file-url');
 const phantom = require('phantom');
 const phantomPath = require('phantomjs-prebuilt').path;
-const promisify = require('util').promisify;
-const im = require('gm').subClass({imageMagick: true});
+const imagemin = require('imagemin');
+const imagemin_pngquant = require('imagemin-pngquant');
+const imagemin_mozjpeg = require('imagemin-mozjpeg');
 const version = require('./package.json').version;
 const MAX_VIEWPORT = {width: 1920, height: 1080};
 
@@ -18,30 +19,38 @@ function sleep(timeout) {
     })
 }
 
-// Make GM use promises
-im.prototype.write = promisify(im.prototype.write);
-
 async function renderPage(page, directory) {
     const uri = fileUrl(path.join(directory, program.index));
     console.log(`Loading ${uri}`);
     const status = await page.open(uri);
     assert(status === 'success');
     await page.property('viewportSize', MAX_VIEWPORT);
+
     await sleep(program.wait * 1000);
+
+    const templateVars = Object.assign({folder: path.basename(directory)}, _.pick(program, ['quality', 'index', 'el', 'wait']));
+    const outputFilename = path.normalize(path.join(directory, outputTemplate(templateVars)));
+    const outputPath = path.dirname(outputFilename);
+    const format = (path.extname(outputFilename).toLowerCase() == '.png') ? 'png' : 'jpeg';
+    shell.mkdir('-p', outputPath);
+    console.log(`Capturing to image ${outputFilename}`);
+
     const bounds = await page.evaluate(function (selector) {
         return document.querySelector(selector).getBoundingClientRect();
     }, program.el);
     await page.property('clipRect', bounds);
-    const templateVars = Object.assign({folder: path.basename(directory)}, _.pick(program, ['quality', 'index', 'el', 'wait']));
-    const outputFilename = path.join(directory, outputTemplate(templateVars));
-    console.log(`Capturing to image ${outputFilename}`);
-    await page.render(outputFilename, {format: 'png'});
-    await im(outputFilename)
-        .setFormat('jpeg')
-        .quality(100)
-        .define(`jpeg:extent=${parseInt(program.targetsize) * 1000}`)
-        .noProfile()
-        .write(outputFilename);
+    await page.render(outputFilename, {format, quality: '100'});
+
+    const destination = path.join(path.dirname(outputFilename), 'optimized');
+    console.log(`Compressing into ${destination}...`);
+
+    await imagemin([outputFilename], {
+        destination,
+        plugins: [
+            imagemin_mozjpeg({quality: program.quality}),
+            imagemin_pngquant({strip: true, quality: [0, program.quality/100.0]}),
+        ]
+    });
 }
 
 async function go(rootDir) {
@@ -66,11 +75,12 @@ program
     .description('Capture a specific HTML element from each file. Start looking in <directory>, usually "."')
     .option('-i, --index <filename.html>', 'Name of HTML file to look for in each directory', 'index.html')
     .option('-e, --el <selector>', 'CSS selector for HTML element to grab', '#container')
-    .option('-o, --output <filename>', 'Output filename pattern, relative to the containing folder. You can use' +
-        'variables in this string such as {folder} for the folder name, and relative paths', '../backup_{folder}.jpg')
+    .option('-o, --output <filename>', 'Output filename pattern, relative to the containing folder. You can use ' +
+        'variables in this string such as {folder} for the folder name, {index} {el} and {wait} from above options. ' +
+        'Supported file types are .png and .jpg.', '../backup_{folder}.jpg')
     .option('-w, --wait <sec>', 'Time to wait in seconds', parseFloat, 0.1)
-    .option('-t, --targetsize <kb>', 'Output to an image with file size equal to the target size in kb', 35)
-    // .option('-q, --quality <0-100>', 'JPG quality', parseFloat, 100)
+    //.option('-t, --targetsize <kb>', 'Output to an image with file size equal to the target size in kb', 35)
+    .option('-q, --quality <0-100>', 'Compressed image quality', parseFloat, 75)
     .parse(process.argv);
 
 // Make a template function out of the passed template string, using single curly braces for variables
